365f21b31f09c495e22f3ba9361dc35e4408bea5
Mikael Gramont <mgramont@gmail.com>
2012-12-17 00:04:28 -0800

    Made kicker remain in a fixed position

--- script.js
+++ script.js
@@ -34,16 +34,29 @@ function update() {
 }
 
 function draw(height, angle, radius, length) {
-
-  var startAngle = Math.PI / 2;
-  var endAngle = startAngle - angle * Math.PI / 180;
+  var canvasHeight = 600;
+  var angleRad = angle * Math.PI / 180;
+  var scale = 100;
+  var steps = 20;
+  var currentAngleRad, x, y;
+  var scaledRadius = radius * scale;
 
   context.clearRect(0, 0, 800, 600);
   context.strokeStyle = '#000000';
+  context.fillStyle = "rgb(200,0,0)";
   context.lineWidth = 2;
   context.beginPath();
   
-  context.arc(200, 200, 200, startAngle, endAngle, true);
+  for (var i = 0; i < steps; i++) {
+    currentAngleRad = i / steps * angleRad;
+    x = scaledRadius * Math.sin(currentAngleRad);
+    y = canvasHeight - scaledRadius * (1 - Math.cos(currentAngleRad));
+    context.lineTo(x, y);
+  } 
+  context.lineTo(x + 20, y);
+  context.lineTo(x + 20, canvasHeight);
+  context.lineTo(0, canvasHeight);
+  context.fill();
 
   context.stroke();
   context.closePath();
