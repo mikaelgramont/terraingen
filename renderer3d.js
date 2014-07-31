02c7c352f1b22b7238c0d6f55c9f8aac78b6729a
mikaelgramont <mgramont@gmail.com>
2014-07-30 22:41:32 -0700

    Added initial support for cannon.js. Kicker now drops from the sky and bounces a bit before giving user orbit control.

--- renderer3d.js
+++ renderer3d.js
@@ -6,7 +6,9 @@ var WebGLRenderer = function(canvasEl, representation, pubsub) {
 	this.representation = representation;
 	this.pubsub = pubsub;
 	this.parts = null;
+	this.lookAtKicker = true;
 	this.init();
+	this.initPhysics();
 
 	// requestAnimationFrame id.
 	this.rafId = null;
@@ -14,6 +16,53 @@ var WebGLRenderer = function(canvasEl, representation, pubsub) {
 	this.pubsub.subscribe("stop-rendering", this.stop.bind(this));
 	this.pubsub.subscribe("resume-rendering", this.render.bind(this));
 };
+
+WebGLRenderer.prototype.initPhysics = function() {
+	var world = new CANNON.World();
+	world.defaultContactMaterial.contactEquationStiffness = 5e7;
+	world.defaultContactMaterial.contactEquationRegularizationTime = 4;
+	world.gravity.set(0, 0, -9.81);
+	world.broadphase = new CANNON.NaiveBroadphase();
+	world.solver.iterations = 20;
+	world.solver.tolerance = 0;
+
+	// Ground plane
+	var groundShape = new CANNON.Plane();
+	var groundBody = new CANNON.RigidBody(0, groundShape);
+	groundBody.position = new CANNON.Vec3(0, 0, 0);
+	var groundMat = new CANNON.Material();
+	groundBody.material = groundMat;
+	world.add(groundBody);
+
+	// Kicker Box
+	var box = new CANNON.Box(new CANNON.Vec3(5, .65, 0));
+	var boxBody = new CANNON.RigidBody(30, box);
+	boxBody.position = new CANNON.Vec3(0, 0, 10);
+	boxBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0,1,0), Math.PI / 180 * -15);
+	var boxMat = new CANNON.Material();
+	boxBody.material = boxMat;
+	world.add(boxBody);
+
+	// Materials
+	var friction = 0.0;
+	var restitution = 0;
+	var kickerGroundContact = new CANNON.ContactMaterial(groundMat, boxMat,
+		friction, restitution);
+	kickerGroundContact.contactEquationStiffness = 1e10;
+	kickerGroundContact.contactEquationRegularizationTime =3;	
+	world.addContactMaterial(kickerGroundContact);
+
+	this.boxBody = boxBody;
+	window.boxBody = boxBody;
+	this.world = world;
+
+	var onCollision = this.useOrbitControl.bind(this);
+	boxBody.addEventListener("collide", function(e) {
+		console.log("collision", e)
+		onCollision();
+	});
+};
+
 
 WebGLRenderer.prototype.render = function() {
 	console.log('WebGLRenderer - rendering');
@@ -31,9 +80,10 @@ WebGLRenderer.prototype.stop = function() {
 WebGLRenderer.prototype.init = function() {
 	var aspectRatio = this.canvasEl.clientWidth / this.canvasEl.clientHeight;
 	this.camera = new THREE.PerspectiveCamera(50, aspectRatio, 1, 100);
-	this.camera.position.x = 0;
-	this.camera.position.y = 1.5;
-	this.camera.position.z = 5;
+	this.camera.position.x = 3;
+	this.camera.position.y = 1.6;
+	this.camera.position.z = 7;
+	// window.camera = this.camera;
 
 	this.scene = new THREE.Scene();
 
@@ -53,8 +103,6 @@ WebGLRenderer.prototype.init = function() {
 	this.threeRenderer.setSize(
 	this.canvasEl.clientWidth, this.canvasEl.clientHeight);
 
-	this.orbitControls = new THREE.OrbitControls(this.camera, this.canvasEl);
-
 	this.setupGroup();
 
 	this.scene.add(new THREE.AxisHelper(1));
@@ -68,13 +116,20 @@ WebGLRenderer.prototype.init = function() {
 	}));
 };
 
+WebGLRenderer.prototype.useOrbitControl = function() {
+	if (!this.lookAtKicker) {
+		return;
+	}
+	this.lookAtKicker = false;
+	this.orbitControls = new THREE.OrbitControls(this.camera, this.canvasEl);
+}
 	
 WebGLRenderer.prototype.setupGroup = function() {
 	// meshGroup contains meshes.
 	// group contains meshGroup and an axis, and is the object
 	// meant to be moved around.
 
-	this.meshGroup = new THREE.Object3D();
+	this.group = new THREE.Object3D();
 	var parts = this.representation.getParts();
 	for (var part in parts) {
 	    if (parts.hasOwnProperty(part)) {
@@ -85,28 +140,37 @@ WebGLRenderer.prototype.setupGroup = function() {
 		    }
 	    }
 	}
-	var helper = new THREE.BoundingBoxHelper(this.meshGroup, 0);
+	var helper = new THREE.BoundingBoxHelper(this.group, 0);
 	helper.update();
-	var xOffset = (helper.box.max.x + helper.box.min.x) * .7;
-	this.meshGroup.translateX(-xOffset);
-	var yOffset = (helper.box.max.y + helper.box.min.y) * .5;
-	this.meshGroup.translateY(-yOffset);
-
-	this.group = new THREE.Object3D();
-	this.group.add(new THREE.AxisHelper(1));
-	this.group.add(this.meshGroup);
-	this.group.translateY(yOffset);
-
-	this.scene.add(this.group);	
+	var xOffset = (helper.box.max.x + helper.box.min.x);
+	this.group.translateX(xOffset);
+	// var yOffset = (helper.box.max.y + helper.box.min.y) * .5;
+	// this.group.translateY(-yOffset);
+	this.scene.add(this.group);
+	window.group = this.group;
 };
 
 WebGLRenderer.prototype.addPart = function(part) {
-	this.meshGroup.add(part.mesh);	
+	this.group.add(part.mesh);	
 }
-
+var ii = 0;
 WebGLRenderer.prototype.animate = function() {
 	var animate = this.animate.bind(this);
 	this.rafId = requestAnimationFrame(animate);
+	this.world.step(1/60);
+	this.group.position.x = this.boxBody.position.x;
+	this.group.position.y = this.boxBody.position.z;
+	this.group.position.z = this.boxBody.position.y;
+
+	if (this.lookAtKicker) {
+		this.camera.lookAt(this.group.position);
+	}
+
+	this.group.quaternion.x = this.boxBody.quaternion.x;
+	this.group.quaternion.y = this.boxBody.quaternion.z;
+	this.group.quaternion.z = this.boxBody.quaternion.y;
+	this.group.quaternion.w = this.boxBody.quaternion.w;
+
 	this.draw();
 };
 
---
+++ threex.grassground.js
@@ -0,0 +1,44 @@
+var THREEx	= THREEx	|| {}
+
+THREEx.GrassGround	= function(opts){
+	// handle default arguments
+	opts		= opts	|| {}
+	var width	= opts.width !== undefined ? opts.width : 1
+	var height	= opts.height !== undefined ? opts.height : 1
+	var segmentsW	= opts.segmentsW !== undefined ? opts.segmentsW : 1
+	var segmentsH	= opts.segmentsH !== undefined ? opts.segmentsH : 1
+	var repeatX	= opts.repeatX !== undefined ? opts.repeatX : 1
+	var repeatY	= opts.repeatY !== undefined ? opts.repeatY : 1
+	var anisotropy	= opts.anisotropy !== undefined ? opts.anisotropy : 16
+
+	// create the textureDiffuse	
+	var textureDiffuseUrl	= './images/grasslight-small.jpg'
+	var textureDiffuse	= THREE.ImageUtils.loadTexture(textureDiffuseUrl);
+	textureDiffuse.wrapS	= THREE.RepeatWrapping;
+	textureDiffuse.wrapT	= THREE.RepeatWrapping;
+	textureDiffuse.repeat.x= repeatX
+	textureDiffuse.repeat.y= repeatY
+	textureDiffuse.anisotropy = anisotropy;
+
+	// create the textureNormal	
+	var textureNormalUrl	= './images/grasslight-small-nm.jpg'
+	var textureNormal	= THREE.ImageUtils.loadTexture(textureNormalUrl);
+	textureNormal.wrapS	= THREE.RepeatWrapping;
+	textureNormal.wrapT	= THREE.RepeatWrapping;
+	textureNormal.repeat.x	= repeatX
+	textureNormal.repeat.y	= repeatY
+	textureNormal.anisotropy= anisotropy;
+
+	// build object3d
+	var geometry	= new THREE.PlaneGeometry(width, height, segmentsW, segmentsH)
+	var material	= new THREE.MeshPhongMaterial({
+		map		: textureDiffuse,
+		normalMap	: textureNormal,
+        normalScale	: new THREE.Vector2(1,1).multiplyScalar(0.5),
+		color		: 0x44FF44,
+	})
+	var object3D	= new THREE.Mesh(geometry, material)
+	object3D.rotateX(-Math.PI/2)
+	// return the just-built object3d
+	return object3D;
+}
